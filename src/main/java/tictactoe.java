d9aa780d514be8e948dece8b5fd902ad4de8e89e
janiesheetal <p.janiesheetal@gmail.com>
2026-04-21 09:15:51 +0530

    Display empty Tic Tac Toe board

---
+++ src/main/java/tictactoe.java
@@ -0,0 +1,22 @@
+public class tictactoe {
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        // Initialize board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print board
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
